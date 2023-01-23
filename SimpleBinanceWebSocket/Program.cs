816e8192fdbd482e0457e4eaf00cf09d89fbf8ea
ignas <ignas@ROOT>
2023-01-23 18:18:52 +0200

    Add project files.

---
+++ SimpleBinanceWebSocket/Program.cs
@@ -0,0 +1,38 @@
+﻿using Binance.Net.Clients;
+using Binance.Net.Enums;
+using CryptoExchange.Net.Authentication;
+
+namespace SimpleBinanceWebSocket;
+
+public class Program
+{
+    static void Main(string[] args) => new Program().Start(args);
+
+    private string _symbol => "BNBUSDT";    // Add symbol pair
+    private string _key => "";              // Add API key
+    private string _secret => "";           // Add Secret key
+    private ApiCredentials _keys => new ApiCredentials(_key, _secret);
+
+    private BinanceClient _client => new BinanceClient();
+    private BinanceSocketClient _socketClient => new BinanceSocketClient();
+
+    public void Start(string[] args)
+    {
+        _client.SetApiCredentials(_keys);
+        _socketClient.SetApiCredentials(_keys);
+        StartSocketLoop(KlineInterval.OneMinute);   // Add kline interval
+        Console.ReadLine();
+    }
+
+    private async void StartSocketLoop(KlineInterval klineInterval)
+    {
+        await _socketClient.SpotStreams.SubscribeToKlineUpdatesAsync(_symbol, klineInterval, dataEvent =>
+        {
+            if (dataEvent.Data.Data.Final)
+            {
+                // Do stuff here
+                Console.WriteLine(dataEvent.Data.Data.ClosePrice);
+            }
+        });
+    }
+}
